c5d331a39e030195af34daab1df052c77f8e2d68
innes <innes@innesui-MacBookAir.local>
2023-12-28 10:39:53 +0900

    first upload

---
+++ 원본/js/index.js
@@ -0,0 +1,31 @@
+function block() {
+    alert('미리보기 페이지에서는 기능이 제한됩니다.');
+}
+
+function mapError() {
+    alert('\'네이버지도\' 혹은 \'카카오맵\'을 클릭해주세요.')
+}
+
+function openNavermap() {
+    location.href = "https://map.naver.com/p/entry/place/202675456?c=15.00,0,0,0,dh"
+}
+
+function openKakaomap() {
+    location.href = "https://map.kakao.com/"
+}
+
+function commentsAdd() {
+    let name = $('#name').val();
+    let password = $('#password').val();
+    let msg = $('#msg').val();
+    
+    let html = `
+    <div class="commentsFirst">
+        <p>${name}</p>
+        <p><span>2023.12.18.</span></p>
+    </div>
+    <div class="commentsSec">${msg}</div>
+    `;
+
+    $('#Comments').append(html);
+}
